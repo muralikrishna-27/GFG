9d9b4405378e45c3df919e13297ad758c035f928
Nallaballe Murali Krishna <muralikrishnanallaballe@gmail.com>
2025-07-31 13:25:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.java
+++ Difficulty: Easy/Missing in Array/missing-in-array.java
@@ -1,40 +1,12 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Geeks {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                arr[i] = Integer.parseInt(str[i]);
-            }
-            System.out.println(new Solution().missingNumber(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    int missingNumber(int arr[]) {
+    int missingNum(int arr[]) {
         // code here
-        int left = 1;
-        int res = 0;
-        Arrays.sort(arr);
-        for(int i = 0; i<arr.length ;i++){
-            if(arr[i] == left){
-                left++;
-                res = left;
-            }else if(arr[i]!=left){
-                res = left;
-            }
+        long n = arr.length+1;
+        double s = n*(n+1)/2;
+        double s1 = 0;
+        for(int a:arr){
+            s1 += a;
         }
-        return res;
+        return (int)(s-s1);
     }
 }
