ee95c2b0c9d1aa19e02e5a9c3172b8d899316ad3
Nallaballe Murali Krishna <muralikrishnanallaballe@gmail.com>
2025-08-04 11:22:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Spiral Matrix/spiral-matrix.java
@@ -0,0 +1,34 @@
+// User function Template for Java
+
+class Solution {
+
+    public int findK(int matrix[][], int n, int m, int k) {
+        // Your code goes here
+                List<Integer> res = new ArrayList<>();
+        // int n = matrix.length;
+        // int m = matrix[0].length;
+        int top =0;
+        int bot = n-1;
+        int left = 0;
+        int right = m-1;
+        while(top<=bot && left<=right){
+            for(int i=left; i<=right; i++)
+                res.add(matrix[top][i]);
+            top++;
+            for(int i=top; i<=bot; i++)
+                res.add(matrix[i][right]);
+            right--;
+            if(top<=bot){
+                for(int i=right; i>=left; i--)
+                    res.add(matrix[bot][i]);
+                bot--;
+            }
+            if(left<=right){
+                for(int i=bot; i>=top; i--)
+                    res.add(matrix[i][left]);
+                left++;
+            }
+        }
+        return res.get(k-1); 
+    }
+}
