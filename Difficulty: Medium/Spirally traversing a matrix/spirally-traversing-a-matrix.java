8e84ebad3888b1cc816c7dd8a018d1cfb9b19463
Nallaballe Murali Krishna <muralikrishnanallaballe@gmail.com>
2025-10-04 16:41:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
+++ Difficulty: Medium/Spirally traversing a matrix/spirally-traversing-a-matrix.java
@@ -2,30 +2,24 @@ class Solution {
     public ArrayList<Integer> spirallyTraverse(int[][] matrix) {
         // code here
         ArrayList<Integer> res = new ArrayList<>();
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int top =0;
-        int bot = n-1;
-        int left = 0;
-        int right = m-1;
+        int n = matrix.length;//row
+        int m = matrix[0].length;//column
+        int top =0, bot = n-1;
+        int left = 0, right = m-1;
         while(top<=bot && left<=right){
-            for(int i=left; i<=right; i++)
-                res.add(matrix[top][i]);
+            for(int i=left; i<=right; i++) res.add(matrix[top][i]);
             top++;
-            for(int i=top; i<=bot; i++)
-                res.add(matrix[i][right]);
+            for(int i=top; i<=bot; i++) res.add(matrix[i][right]);
             right--;
             if(top<=bot){
-                for(int i=right; i>=left; i--)
-                    res.add(matrix[bot][i]);
+                for(int i=right; i>=left; i--) res.add(matrix[bot][i]);
                 bot--;
             }
             if(left<=right){
-                for(int i=bot; i>=top; i--)
-                    res.add(matrix[i][left]);
+                for(int i=bot; i>=top; i--) res.add(matrix[i][left]);
                 left++;
             }
         }
-        return res; 
+        return res;
     }
 }
