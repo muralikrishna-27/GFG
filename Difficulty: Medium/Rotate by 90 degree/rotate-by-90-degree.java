78a2f257b21d1c21dacdcbc11b0fc3f5f758b4e4
Nallaballe Murali Krishna <muralikrishnanallaballe@gmail.com>
2026-02-18 11:40:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.java
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.java
@@ -1,16 +1,26 @@
-// User function Template for Java
+class Solution {
+    static void rotateMatrix(int[][] mat) {
+        int n = mat.length;
+        
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int start = 0, end = n - 1;
+            while (start < end) {
+                int temp = mat[i][start];
+                mat[i][start] = mat[i][end];
+                mat[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
 
-class GFG {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for (int i = 0; i < n; i++)
-            for (int j = i; j < n; j++) swap(matrix, i, j, j, i);
-        for (int i = 0; i < n; i++) 
-            for (int j = 0; j < n / 2; j++) swap(matrix, i, j, i, n - 1 - j);
-    }
-    private void swap(int[][] matrix, int i1,int j1,int i2,int j2){
-        int temp = matrix[i1][j1];
-        matrix[i1][j1] = matrix[i2][j2];
-        matrix[i2][j2] = temp;
+        // Performing Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
     }
 }
