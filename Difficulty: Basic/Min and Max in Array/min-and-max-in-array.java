74c674b6b631bc8aa773a8e94f0ef828a6f8e064
Nallaballe Murali Krishna <muralikrishnanallaballe@gmail.com>
2026-05-03 09:06:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.java
@@ -1,42 +1,18 @@
-// User function Template for Java
-// User function Template for Java
-
-/*
-class Pair<K, V> {
-    private final K key;
-    private final V value;
-
-    public Pair(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
-    }
-}
-
-Java users need to return result in Pair class
-For Example -> return new Pair(minimum,maximum)
-*/
+import java.util.*;
 
 class Solution {
-    public Pair<Integer, Integer> getMinMax(int[] arr) {
-        // Code Here
-        int large = Integer.MIN_VALUE;
-        int small = Integer.MAX_VALUE;
-        for(int i =0; i<arr.length; i++){
-            if(large<arr[i]){
-                large = arr[i];
-            }
-            if(small>arr[i]){
-                small = arr[i];
-            }
+    public ArrayList<Integer> getMinMax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max) max = arr[i];
+            if(arr[i] < min) min = arr[i];
         }
-        return new Pair(small,large);
+
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(min);
+        res.add(max);
+        return res;
     }
 }
