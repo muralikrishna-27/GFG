319bc8a8c78056f2c462dcede6c61c88d0bb9f7b
Nallaballe Murali Krishna <muralikrishnanallaballe@gmail.com>
2026-05-09 10:11:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Replace a word/replace-a-word.java
@@ -0,0 +1,7 @@
+// User function Template for Java
+class Solution {
+    static String replaceAll(String str, String oldW, String newW) {
+        // code here
+        return str.replace(oldW,newW);
+    }
+}
